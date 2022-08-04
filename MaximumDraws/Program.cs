037afcd95ef9e67a01764417bc402c4da982faa8
James Absalem Lintu <jamesabsalem@gmail.com>
2022-08-05 00:20:59 +0600

    Add project files.

---
+++ MaximumDraws/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximumDraws
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1, 2 };
+            foreach (var i in list)
+            {
+                var x=  maximumDraws(i);
+                Console.WriteLine(x);
+            }
+        }
+        public static int maximumDraws(int n)
+        {
+            return 1 + n;
+        }
+    }
+}
